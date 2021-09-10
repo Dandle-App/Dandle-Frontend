2641faf7bb7fff5c39e7b045064383b022fa60bd
mbami Luka <mbamiluka@gmail.com>
2021-09-10 17:48:29 -0600

    Made proper folder Structure

--- App.js
+++ App.js
@@ -1,22 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { StyledContainer } from './styles/styles.js';
+
+//screens
+import Welcome from './screens/Welcome';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>My name is..Mbami! And this is the Dandle app.
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    <Welcome />
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ddd',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
