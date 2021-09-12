276d3190755dadc480044aab6e1896ef27adbbef
mbami Luka <mbamiluka@gmail.com>
2021-09-11 22:01:41 -0600

    Folder structture + styles.js file

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { Text } from "react-native";
-import { StyledContainer, InnerContainer, Header1,
-        AppLogo } from "../styles/styles";
+import { StatusBar } from 'expo-status-bar';
+import { 
+        StyledContainer, InnerContainer, Header1, 
+        Header2, Header3, AppLogo, StlyedButton,
+        Pad_h_medium } from "../styles/styles";
 
-const logo_img = require("../assets/logo.png");
+const logo_img = require("../assets/logo_silv.png");
 
 const Welcome = () => {
   return (
     <StyledContainer>
+      <StatusBar style="auto" />
       <InnerContainer>
         <AppLogo source = {logo_img}/>
-        <Text>Welcome to Dandle</Text>
-        <Text>Tell your hospital what you need</Text>
+        <Header1>Welcome to Dandle</Header1>
+        <Header2>Swipe to see how it works</Header2>
+        <Pad_h_medium/><Pad_h_medium/><Pad_h_medium/>
+        <StlyedButton><Text>Continue</Text></StlyedButton>
+        <Header3>I am an organization/company</Header3>
       </InnerContainer>
     </StyledContainer>
   );
--- styles/styles.js
+++ styles/styles.js
@@ -1,43 +1,74 @@
 import styled from "styled-components";
 import Constants from 'expo-constants';
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 
 const status_bar_height = Constants.statusBarHeight + 10;
 // App colors
 export const colors = {
-    background: '#ffffff',
-    primary: '#00bcd4',
-    secondary: '#ff9800',
+    background: '#630101',
+    primary: '#f38484',
+    secondary: '#d64547',
     text: '#212121',
     textLight: '#757575',
     textDark: '#212121',
 };
 const { background, primary, secondary, text, 
         textLight, textDark } = colors;
-
+        
+//container styles
 export const StyledContainer = styled.View`
     flex: 1;
     padding-top: ${status_bar_height}px;
     background-color: ${background};
-    padding:25px;
+    padding:24px;
 `
 
 export const InnerContainer = styled.View`
     flex: 1;
     width: 100%;
     align-items: center;
-    padding-top: ${status_bar_height}px;
+    padding-top: 75%;
 `;
-
+// Gaps
+export const Padding_h_small = styled.View`
+    height: 10px;
+`;
+export const Pad_h_medium = styled.View`
+    height: 45px;
+`;
+// text styles
 export const Header1 = styled.Text`
-    font-size: 24px;
+    font-size: 20px;
     font-weight: bold;
     color: ${primary};
-    padding: 10px;
+    padding-top: 42px;
 `;
+export const Header2 = styled.Text`
+    font-size: 12px;
+    font-weight: bold;
+    color: ${primary};
+    padding-top: 5px;
+    padding-bottom: 10px;
+`;
+export const Header3 = styled.Text`
+    font-size: 12px;
+    font-weight: bold;
+    color: ${primary};
+    position: absolute;
+    bottom: 0;
+`;
+//img styles
 export const AppLogo = styled.Image`
-    width: 100px;
-    height: 100px;
+    width: 66px;
+    height: 77px;
 `;
 
-<img src='../assets/im.png'/>
+//button styles
+export const StlyedButton = styled.TouchableOpacity`
+    background-color: ${secondary};
+    align-items: center;
+    justifyContent: space-around;
+    width: 150px
+    height: 50px;
+    border-radius: 10px;
+`;
