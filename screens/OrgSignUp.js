e39d52fcfe7e84278be07f52e0494e8fdb228205
Bradley Isaacs <bisaacs3@msudenver.edu>
2021-09-30 10:32:27 -0600

    Updated OrgSignUp handleSubmit
    added:                                     
    onChangeText={handleChange("??") 
    value={values.??}
    to each textInput

--- screens/OrgSignUp.js
+++ screens/OrgSignUp.js
@@ -18,9 +18,7 @@ const logo_img = require("../assets/logo_red.png");
 const OrgSignUp = ({navigation}) => {
   //write a handleSubmit function
   const handleSubmit = (values) => {
-    values = {companyName: "Dandle Group", companyEmail: "dandle@msudenver.edu", companyPhoneNumber: "303", orgCode: "dandle",
-              password: "dandle", address: "777 Lawrence St.", city: "Denver",
-              state: "CO", country: "United States", zipCode: "80204"}
+
     console.log(values);
     const url = "http://localhost:3000/v1/orgs/signup";
     axios.post(url, values)
@@ -30,96 +28,117 @@ const OrgSignUp = ({navigation}) => {
   }
   return (
     <KeyboardAvoidingWrapper>
-    <LightContainer>
-        <StatusBar style="dark" />
-        <PadlessContainer>
-            <Formik
-                initialValues={{
-                    companyName: "",
-                    companyEmail: "",
-                    companyPhoneNumber: "",
-                    orgCode: "",
-                    password: "",
-                    confirmPassword: "",
-                    address: "",
-                    city: "",
-                    state: "",
-                    country: "",
-                    zipCode: ""
-                }}
-                onSubmit={(values) => {
-                  navigation.navigate("OrgSignIn");
-                  handleSubmit(values);
-                }}
-            >
-                {
-                    ({handleChange, handleBlur, handleSubmit, values}) => (
-                        <StyledFormArea>
-                            <StyldTextInput
-                                label="Company Name"
-                                placeholder="John Doe"
-                                onChangeText={handleChange('name')}
-                            />
-                            <StyldTextInput
-                                label="Company Email"
-                                placeholder="johndoe@acme.com"
-                                keyboardType="email-address"
-                            />
-                            <StyldTextInput
-                                label="Company Phone No"
-                                placeholder="303 309 9200"
-                            />
-                            <StyldTextInput
-                                label="Org Code"
-                                placeholder="ACME101"
-                                autoCapitalize="characters"
-                            />
-                            <StyldTextInput
-                                label="Password"
-                                placeholder="* * * * * * *"
-                                secureTextEntry={true}
-                            />
-                            <StyldTextInput
-                                label="Confirm Password"
-                                placeholder="* * * * * * *"
-                                secureTextEntry={true}
-                            />
-                            <StyldTextInput
-                                label="Address"
-                                placeholder="123 Main St"
-                            />
-                            <FlexHoriztal>
-                              <StyldTextInput
-                                  label="City"
-                                  placeholder="Denver"
-                              />
-                              <StyldTextInput
-                                  label="State"
-                                  placeholder="CO"
-                              />
-                            </FlexHoriztal>
-                            <FlexHoriztal>
-                              <StyldTextInput
-                                  label="Country"
-                                  placeholder="United States"
-                              />
-                              <StyldTextInput
-                                  label="Zip Code"
-                                  placeholder="90210"
-                              />
-                            </FlexHoriztal>
-                            <FlexHoriztal>
-                                <StlyedButton onPress={handleSubmit}>
-                                    <TextLight>sign up</TextLight>
-                                </StlyedButton>
-                            </FlexHoriztal>
-                        </StyledFormArea>
-                    )
-                }
-            </Formik>    
-        </PadlessContainer>
-    </LightContainer>
-</KeyboardAvoidingWrapper>
+        <LightContainer>
+            <StatusBar style="dark" />
+            <PadlessContainer>
+                <Formik
+                    initialValues={{
+                        companyName: "",
+                        companyEmail: "",
+                        companyPhoneNumber: "",
+                        orgCode: "",
+                        password: "",
+                        confirmPassword: "",
+                        address: "",
+                        city: "",
+                        state: "",
+                        country: "",
+                        zipCode: ""
+                    }}
+                    onSubmit={(values) => {
+                    navigation.navigate("OrgSignIn");
+                    handleSubmit(values);
+                    }}
+                >
+                    {
+                        ({handleChange, handleBlur, handleSubmit, values}) => (
+                            <StyledFormArea>
+                                <StyldTextInput
+                                    label="Company Name"
+                                    placeholder="John Doe"
+                                    onChangeText={handleChange("companyName")}
+                                    value={values.companyName}
+                                />
+                                <StyldTextInput
+                                    label="Company Email"
+                                    placeholder="johndoe@acme.com"
+                                    keyboardType="email-address"
+                                    onChangeText={handleChange("companyEmail")}
+                                    value={values.companyEmail}
+                                />
+                                <StyldTextInput
+                                    label="Company Phone No"
+                                    placeholder="303 309 9200"
+                                    onChangeText={handleChange("companyPhoneNumber")}
+                                    value={values.companyPhoneNumber}
+                                />
+                                <StyldTextInput
+                                    label="Org Code"
+                                    placeholder="ACME101"
+                                    autoCapitalize="characters"
+                                    onChangeText={handleChange("orgCode")}
+                                    value={values.orgCode}
+                                />
+                                <StyldTextInput
+                                    label="Password"
+                                    placeholder="* * * * * * *"
+                                    secureTextEntry={true}
+                                    onChangeText={handleChange("password")}
+                                    value={values.password}
+                                />
+                                <StyldTextInput
+                                    label="Confirm Password"
+                                    placeholder="* * * * * * *"
+                                    secureTextEntry={true}
+                                    onChangeText={handleChange("confirmPassword")}
+                                    value={values.confirmPassword}
+                                />
+                                <StyldTextInput
+                                    label="Address"
+                                    placeholder="123 Main St"
+                                    onChangeText={handleChange("address")}
+                                    value={values.address}
+                                />
+                                <FlexHoriztal>
+                                <StyldTextInput
+                                    label="City"
+                                    placeholder="Denver"
+                                    onChangeText={handleChange("city")}
+                                    value={values.city}
+                                />
+                                <StyldTextInput
+                                    label="State"
+                                    placeholder="CO"
+                                    onChangeText={handleChange("state")}
+                                    value={values.state}
+                                />
+                                </FlexHoriztal>
+                                <FlexHoriztal>
+                                <StyldTextInput
+                                    label="Country"
+                                    placeholder="United States"
+                                    onChangeText={handleChange("country")}
+                                    value={values.country}
+                                />
+                                <StyldTextInput
+                                    label="Zip Code"
+                                    placeholder="90210"
+                                    onChangeText={handleChange("zipCode")}
+                                    value={values.zipCode}
+                                />
+                                </FlexHoriztal>
+                                <FlexHoriztal>
+                                    <StlyedButton onPress={handleSubmit}>
+                                        <TextLight>sign up</TextLight>
+                                    </StlyedButton>
+                                </FlexHoriztal>
+                            </StyledFormArea>
+                        )
+                    }
+                </Formik>    
+            </PadlessContainer>
+        </LightContainer>
+    </KeyboardAvoidingWrapper>
   );
 }
 
