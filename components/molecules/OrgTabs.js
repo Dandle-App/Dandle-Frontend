70ccd048a865f770b712b88ec593098460392ca3
Bradley Isaacs <bradk.isaacs@gmail.com>
2021-10-29 10:58:24 -0600

    Added styling to OrgTabs.js
    Added background color and radius to full tabBar
    Added an Image Icon to each tab: Settings, Home, Chat, and Analytics.

--- components/molecules/OrgTabs.js
+++ components/molecules/OrgTabs.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
@@ -7,18 +7,84 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import OrgSettings from '../../screens/org/OrgSettings';
 import OrgHomeScreen from '../../screens/org/OrgHomeScreen';
 import OrgAnalytics from '../../screens/org/OrgAnalytics';
-import OrgChat from '../../screens/org/OrgChat';
+import OrgChat from '../../screens/org/OrgChat'; 
+
+//icons
+const SETTINGS = require('../../assets/settings.png');
+const HOME = require('../../assets/home.png');
+const CHAT = require('../../assets/chat.png');
+const ANALYTICS = require('../../assets/analytics.png');
 
 const Tab = createBottomTabNavigator();
 
 export default function OrgTabs() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={OrgHomeScreen} />
-        <Tab.Screen name="Chat" component={OrgChat} />
-        <Tab.Screen name="Settings" component={OrgSettings} />
-        <Tab.Screen name="Analytics" component={OrgAnalytics} />
+      <Tab.Navigator
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            position: 'absolute', 
+            backgroundColor: '#630101', 
+            borderRadius: 100}}
+        }
+      >
+        <Tab.Screen 
+          name="Settings" 
+          component={OrgSettings}
+          options={{
+            tabBarIcon: ({size,focused,color}) => {
+              return(
+                <Image 
+                  style={{width: size, height: size}}
+                  source={SETTINGS}
+                />
+              )
+            }
+          }}
+        />
+        <Tab.Screen 
+          name="Home" 
+          component={OrgHomeScreen}
+          options={{
+            tabBarIcon: ({size,focused,color}) => {
+              return(
+                <Image 
+                  style={{width: size, height: size}}
+                  source={HOME}
+                />
+              )
+            }
+          }} 
+        />
+        <Tab.Screen 
+          name="Chat" 
+          component={OrgChat} 
+          options={{
+            tabBarIcon: ({size,focused,color}) => {
+              return(
+                <Image 
+                  style={{width: size, height: size}}
+                  source={CHAT}
+                />
+              )
+            }
+          }}
+        />
+        <Tab.Screen 
+          name="Analytics" 
+          component={OrgAnalytics} 
+          options={{
+            tabBarIcon: ({size,focused,color}) => {
+              return(
+                <Image 
+                  style={{width: size, height: size}}
+                  source={ANALYTICS}
+                />
+              )
+            }
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
