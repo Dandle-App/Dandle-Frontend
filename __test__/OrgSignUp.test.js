185a35b7221d44901c66b78d906d5c917d5b0e4e
GregDews <gregdews@gmail.com>
2021-11-02 00:05:28 -0600

    screen rendering test, needs correct props

---
+++ __test__/OrgSignUp.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {create, act} from 'react-test-renderer';
+import 'jest-styled-components'
+import OrgSignUp from '../screens/OrgSignUp';
+
+const createTestProps = (props) => ({
+  navigation: {
+    navigate: jest.fn()
+  },
+  handleSubmit: {
+    values: jest.values
+  },
+  ...props
+});
+
+/* 
+Jest Snapshots : https://jestjs.io/docs/snapshot-testing
+To update a snapshot due to changed implementation: jest --updateSnapshot
+*/
+// async snapshot
+it('renders correctly', () => {
+    let tree 
+    let props
+    beforeEach(() => {
+        props = createTestProps({});
+    });
+    tree = renderer.create(<OrgSignUp {...props} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
