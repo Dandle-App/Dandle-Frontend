23a269cf37fe87006ce308ed98f913446d92ed5c
GregDews <gregdews@gmail.com>
2021-11-02 02:25:37 -0600

    reverted to original snapshot method

--- __test__/OrgSignUp.test.js
+++ __test__/OrgSignUp.test.js
@@ -3,27 +3,14 @@ import {renderer} from 'react-test-renderer';
 import 'jest-styled-components'
 import OrgSignUp from '../screens/OrgSignUp';
 
-const createTestProps = (props) => ({
-  navigation: {
-    navigate: jest.fn()
-  },
-  handleSubmit: {
-    values: jest.values
-  },
-  ...props
-});
-
 /* 
 Jest Snapshots : https://jestjs.io/docs/snapshot-testing
 To update a snapshot due to changed implementation: jest --updateSnapshot
 */
-// async snapshot
-it('renders correctly', () => {
-    let tree 
-    let props
-    beforeEach(() => {
-        props = createTestProps({});
-    });
-    tree = renderer.create(<OrgSignUp {...props} />).toJSON();
-    expect(tree).toMatchSnapshot();
+
+describe('OrgSignUp Snapshot', () => {
+  it('renders correctly', () => {
+      const tree = renderer.create(<OrgSignUp />).toJSON();
+      expect(tree).toMatchSnapshot();
   });
+})
