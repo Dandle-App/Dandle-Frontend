8fb643ec8ea886279d53600a573d9dfe63c8cd85
GregDews <gregdews@gmail.com>
2021-11-03 13:17:21 -0600

    Implemented OrgSignUp Snapshot

--- __test__/OrgSignUp.test.js
+++ __test__/OrgSignUp.test.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import {renderer} from 'react-test-renderer';
+import {cleanup, render, fireEvent, waitFor} from '@testing-library/react-native';
+import { toHaveStyle } from "@testing-library/jest-native";
+import styled from 'styled-components';
+import NavigationContainer from '@react-navigation/native';
+import renderer from 'react-test-renderer';
 import 'jest-styled-components'
 import OrgSignUp from '../screens/OrgSignUp';
 
@@ -7,10 +11,11 @@ import OrgSignUp from '../screens/OrgSignUp';
 Jest Snapshots : https://jestjs.io/docs/snapshot-testing
 To update a snapshot due to changed implementation: jest --updateSnapshot
 */
+afterEach(cleanup);
 
 describe('OrgSignUp Snapshot', () => {
   it('renders correctly', () => {
       const tree = renderer.create(<OrgSignUp />).toJSON();
       expect(tree).toMatchSnapshot();
   });
-})
+});
