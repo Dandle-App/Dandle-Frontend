d0be882b8aceac8a00b5a87c191e7ff5a11a4a18
GregDews <gregdews@gmail.com>
2021-11-04 01:39:20 -0600

    Molecules render checks made

--- __test__/Molecules.test.js
+++ __test__/Molecules.test.js
@@ -10,15 +10,63 @@ import { StyldTextInput, OrderCard } from '../components/molecules/Molecules';
 afterEach(cleanup);
 
 describe('Molecules', () => {
-
-    it('StyledTextInput', () =>{
-        const tree = renderer.create(
-            <div>
-            <MsgBox type="success">success</MsgBox>
-            <MsgBox type="fail">fail</MsgBox>
-            </div>
-          ).toJSON();
-          expect(tree).toMatchSnapshot();
-
+    describe("StyledTextInput", () => {
+        it('renders correctly', () =>{
+            const tree = renderer.create(
+                <div>
+                    <StyldTextInput
+                        label="Username"
+                        placeholder="johndoe"
+                        value="FakeName"
+                    />
+                    <StyldTextInput
+                        label="Hidden Password"
+                        placeholder="* * * * * * *"
+                        secureTextEntry={true}
+                        value="fakePassword"
+                        isPassword={true}
+                        hidePassword={true}
+                        setHidePassword={true}
+                    />
+                    <StyldTextInput
+                        label="Plain Password"
+                        placeholder="* * * * * * *"
+                        secureTextEntry={true}
+                        value="fakePassword"
+                        isPassword={true}
+                        hidePassword={false}
+                        setHidePassword={false}
+                    />
+                    <StyldTextInput
+                        label="Hidden Password"
+                        placeholder="* * * * * * *"
+                        secureTextEntry={true}
+                        value="fakePassword"
+                        isPassword={true}
+                        hidePassword={false}
+                        setHidePassword={true}
+                    />
+                </div>
+            ).toJSON();
+            expect(tree).toMatchSnapshot();
+        });
+    });
+    describe("OrderCard", () => {
+        it("renders correctly", () => {
+            const tree = renderer.create(
+                <div>
+                <OrderCard
+                    key={'20180101203000'}
+                    order = {
+                        orderDate = '2018-01-01:20:30:00',
+                        orderStatus = 'Pending',
+                        customerName = 'Mbami Luka',
+                        orderTotal = '$1,000.00'
+                    }
+                />
+                </div>
+            ).toJSON();
+            expect(tree).toMatchSnapshot();
+        });
     });
 });
